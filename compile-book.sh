3f4800455cf4292abc7d18f995ae20fb341e80f2
Sam Burnstone <sburnstone@scottlogic.com>
2016-04-22 17:25:35 +0100

    cleans up script

--- compile-book.sh
+++ compile-book.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+# Extract and format chapter number and title
+extract_chapter_title()
+{
+	chapter_filename=$(basename "$1")
+	echo "#${chapter_filename%.*}"
+}
+
 text=""
 
+# Iterate through all files to compile them into a single file
 for file in chapters/*; do
-file_contents=$(cat "$file")
-chapter_title=$(basename "$file")
-text="$text$chapter_title\n\n$file_contents\n\n"
+	file_contents=$(cat "$file")
+	chapter_title=$(extract_chapter_title "$file")
+	all_chapter_content="$chapter_title\n\n$file_contents\n\n"
+	text="$text$all_chapter_content"
 done
 
 output_file=output/output.html
 
+# Pass concatenated chapters to markdown parser
 echo "$text" | python -m markdown > "$output_file"
 
-echo "\nMarkdown conversion complete. Output located in $output_file"
+echo "Markdown conversion complete. Output located in $output_file"
