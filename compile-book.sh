4305b07dca1a127652081ad14db797a49a8b8526
Sam Burnstone <sburnstone@scottlogic.com>
2016-04-22 17:44:19 +0100

    Updates script to use pipelines rather than variables
    Reduces the complexity of the script and makes it easier to read.

--- compile-book.sh
+++ compile-book.sh
@@ -7,19 +7,14 @@ extract_chapter_title()
 	echo "#${chapter_filename%.*}"
 }
 
-text=""
+output_file=output/output.html
 
 # Iterate through all files to compile them into a single file
 for file in chapters/*; do
-	file_contents=$(cat "$file")
-	chapter_title=$(extract_chapter_title "$file")
-	all_chapter_content="$chapter_title\n\n$file_contents\n\n"
-	text="$text$all_chapter_content"
-done
-
-output_file=output/output.html
-
-# Pass concatenated chapters to markdown parser
-echo "$text" | python -m markdown > "$output_file"
+	extract_chapter_title "$file"
+	printf "\n\n"
+	cat "$file"
+	printf "\n\n"
+done | python -m markdown > "$output_file"
 
 echo "Markdown conversion complete. Output located in $output_file"
