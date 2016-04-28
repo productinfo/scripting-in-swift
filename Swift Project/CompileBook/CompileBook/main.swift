b19f9c35b87c9398d667ee11fced1e75e30d07d4
Sam Burnstone <sburnstone@scottlogic.com>
2016-04-28 10:42:10 +0100

    Adds CommandLine tool project for swift scripting project

---
+++ Swift Project/CompileBook/CompileBook/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CompileBook
+//
+//  Created by Samuel Burnstone on 28/04/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import Foundation
+
+print("Compile the book!")
