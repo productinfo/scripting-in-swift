7cc975f94c28557538495789ccf6fd4d2eff839f
Sam Burnstone <sburnstone@scottlogic.com>
2016-05-03 11:56:38 +0100

    Renames 'Swift Project' to 'Swift-Project'

--- Swift-Project/CompileBook/Testing/Testing.swift
+++ Swift-Project/CompileBook/Testing/Testing.swift
@@ -8,19 +8,10 @@
 
 import XCTest
 
-class Testing: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
+class Testing: XCTestCase
+{
     /** Simple tests to ensure we're calling through to markdown-parsing libraries successfully */
+    
     func test_markdown_is_converted_into_html_using_python_library()
     {
         let markdown = "# This is a title"
